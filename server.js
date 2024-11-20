1819125bdf8be16fae44ebd3002192fe35cc0997
Paulwq12 <gracetacha1@gmail.com>
2024-11-20 14:17:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3010;
 
 // Middleware
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from public directory
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
