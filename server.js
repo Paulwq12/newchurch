62c8be341955a39c6c70fd57c61a04ea999a5517
Paulwq12 <gracetacha1@gmail.com>
2024-11-20 15:00:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,9 +18,7 @@ const loadJSON = (filePath) => JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 const saveJSON = (filePath, data) => fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 
 // Routes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+app.get('/', (req, res) => res.render('index'));
 
 app.get('/login', (req, res) => res.render('login'));
 
